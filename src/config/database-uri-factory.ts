019f6ecf97a47c676c0a8a71617d516c8929ccd2
Daniel Cornock <daniel_cornock@hotmail.co.uk>
2021-02-23 00:49:44 +0000

    task(*): ensure db uri is available on prod

--- src/config/database-uri-factory.ts
+++ src/config/database-uri-factory.ts
@@ -1,13 +1,17 @@
 import { Logger } from '@nestjs/common';
 import { MongooseModuleOptions } from '@nestjs/mongoose';
 
-import { devDatabaseUriString, isDevelopment } from './env';
+import { devDatabaseUriString, isDevelopment, isProduction } from './env';
 
 export function databaseUriFactory(): MongooseModuleOptions {
   if (isDevelopment) {
     Logger.log('Connecting to DEVELOPMENT database', 'DatabaseFactory');
     return {
-      uri: devDatabaseUriString,
+      uri: devDatabaseUriString
+    };
+  } else if (isProduction) {
+    return {
+      uri: devDatabaseUriString
     };
   } else {
     throw new Error('Invalid environment set. Unable to connect to database.');
